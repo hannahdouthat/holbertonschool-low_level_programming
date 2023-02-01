6a5943a5b748c880acf5f58126ae47bc9f11b75a
Hannah Douthat <hdouthat101@gmail.com>
2023-01-31 19:19:30 -0800

    Lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * @i: Something
  * print_last_digit - Prints last digit
+ *@i: Something goes here
  *
  * Return: Value
  */
 
 int print_last_digit(int i)
 {
-	int last;
-	last = i % 10;
-	if (last <0)
-	{last = last * -1;
-	}
-	_putchar(last + '0');
+int last;
+last = i % 10;
 
-	return (last);
+if (last < 0)
+{last = last * -1;
+}
+_putchar(last + '0');
+
+return (last);
 }
