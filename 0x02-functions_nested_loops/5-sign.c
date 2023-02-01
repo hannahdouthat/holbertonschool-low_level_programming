ce620b14a3b7728293bc61176ad5d082ec80bf66
Hannah Douthat <hdouthat101@gmail.com>
2023-01-31 16:38:58 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * main - function print_sign
- */**
  *print_sign - Prints the sign of a number.
  *@n: The number of which the sign will be printed.
  *Return: 1 if the number is greater than zero,
