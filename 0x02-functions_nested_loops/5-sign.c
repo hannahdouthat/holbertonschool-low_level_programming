5864a5dbd9c38e336db04796222407eabcf48821
Hannah Douthat <hdouthat101@gmail.com>
2023-01-31 16:24:00 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,19 +2,19 @@
 
 int print_sign(int i)
 {
-	if (i > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (i < 0)
-	{
-		_putchar('-');
-		return (1);
-	}
-	else 
-	{
-	_putchar ('0');
-		return (0);
-	}
+if (i > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (i < 0)
+{
+_putchar('-');
+return (1);
+}
+else
+{
+_putchar ('0');
+return (0);
+}
 }
