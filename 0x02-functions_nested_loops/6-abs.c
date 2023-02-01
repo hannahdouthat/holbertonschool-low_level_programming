5b7dfd09185b735724913a0b87312df23724b9f2
Hannah Douthat <hdouthat101@gmail.com>
2023-01-31 18:49:22 -0800

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include main.h
+
+/**
+ * @i: is a variable
+ * Return: Return is n
+ */
+
+int_abs(int i)
+{
+if (i>0)
+{
+return (i);
+}
+else if (i<0)
+{
+return (-i);
+}
+else
+{
+return (i);
+}
+}
